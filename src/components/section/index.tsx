1f5647128f8252302372c336f9b04f483316d4d3
François Moine <francois.moine@epitech.eu>
2021-02-22 22:26:38 +0100

    [*] add title in section component

--- src/components/section/index.tsx
+++ src/components/section/index.tsx
@@ -2,16 +2,20 @@
 import React from 'react';
 
 // Styles
-import { Section as Root } from './styles';
+import { Section as Root, Heading1 } from './styles';
 
 interface IProps {
-	children: React.ReactNode
+	children: React.ReactNode,
+	title: React.ReactNode
 }
 
-function Section ({ children }: IProps) {
+function Section ({ children, title }: IProps) {
 
 	return (
 		<Root>
+			<Heading1>
+				{title}
+			</Heading1>
 			{children}
 		</Root>
 	);
--- src/components/section/styles.tsx
+++ src/components/section/styles.tsx
@@ -5,4 +5,11 @@ export const Section = styled.div`
 	margin: auto;
 	height: 100%;
 	max-width: 124rem;
+
+	display: flex;
+	flex-direction: column;
+`;
+
+export const Heading1 = styled.h1`
+	text-align: center;
 `;
