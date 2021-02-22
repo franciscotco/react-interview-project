49c3f4a5f1cabcba5c4a44af03702dee3ab7c830
François Moine <francois.moine@epitech.eu>
2021-02-22 19:15:49 +0100

    [+] create post-list component

---
+++ src/components/post-list/styles.tsx
@@ -0,0 +1,18 @@
+// Vendors
+import styled from 'styled-components';
+
+export const PostList = styled.ul`
+
+`;
+
+export const PostItem = styled.li`
+
+`;
+
+export const Title = styled.div`
+
+`;
+
+export const Body = styled.div`
+
+`;
