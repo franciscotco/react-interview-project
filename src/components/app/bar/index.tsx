59a2d76cf6ab16a7d19574233cc3a390c6eb4989
François Moine <francois.moine@epitech.eu>
2021-02-22 21:19:30 +0100

    [*] improve style of bar component

--- src/components/app/bar/index.tsx
+++ src/components/app/bar/index.tsx
@@ -1,7 +1,8 @@
 // Vendors
 import React from 'react';
 import { useIntl } from 'react-intl';
-import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { NavLink, useLocation } from 'react-router-dom';
 
 // Data
 import { supportedLanguages } from 'data';
@@ -9,8 +10,15 @@ import { supportedLanguages } from 'data';
 // Types
 import IRoutes from 'types/routes';
 
+// Store
+import { ILangState } from 'store/lang';
+
 // Styles
-import { Bar as Root, Part, Link } from './styles';
+import {
+	Bar as Root, Part,
+	RouteList, RouteLink, RouteItem, Heading3,
+	LangList, LangItem, LangLink
+} from './styles';
 
 interface IProps {
 	onSwitchLang: (arg0: string) => void,
@@ -20,30 +28,47 @@ interface IProps {
 function AppBar ({ onSwitchLang, routes }: IProps) {
 
 	const intl = useIntl();
+	const location = useLocation();
+	const lang = useSelector(({ lang }: { lang: ILangState }) => lang);
+
+	console.log(lang);
+
 	const onClick = (lang: string) => () => onSwitchLang(lang);
 
 	return (
 		<Root>
 			<Part>
-				{routes.map(route => (
-					<Link
-						as={NavLink}
-						key={route.path}
-						to={route.path}
-					>
-						{intl.formatMessage({ id: `ROUTE.${route.name.toUpperCase()}.NAME` })}
-					</Link>
-				))}
+				<RouteList>
+					{routes.map(route => (
+						<RouteItem key={route.id}>
+							<RouteLink
+								as={NavLink}
+								to={route.path}
+								$is_active={!!location.pathname.includes(route.path)}
+							>
+								<Heading3>
+									{intl.formatMessage({ id: `ROUTE.${route.name.toUpperCase()}.NAME` })}
+								</Heading3>
+							</RouteLink>
+						</RouteItem>
+					))}
+				</RouteList>
 			</Part>
 			<Part>
-				{supportedLanguages.map(lang => (
-					<Link
-						key={lang}
-						onClick={onClick(lang)}
-					>
-						{lang}
-					</Link>
-				))}
+				<LangList>
+					{supportedLanguages.map(lang_code => (
+						<LangItem
+							key={lang_code}
+						>
+							<LangLink
+								onClick={onClick(lang_code)}
+								is_active={lang_code === lang.code}
+							>
+								{lang_code.toUpperCase()}
+							</LangLink>
+						</LangItem>
+					))}
+				</LangList>
 			</Part>
 		</Root>
 	);
--- src/components/app/bar/styles.tsx
+++ src/components/app/bar/styles.tsx
@@ -1,16 +1,88 @@
 // Vendors
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Bar = styled.div`
+	width: 100%;
+	height: 9rem;
+	box-sizing:border-box;
+
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
-`;
 
-export const Link = styled.a`
+	box-shadow:0 0 0.75rem 0 rgba(0,0,0,.05);
+	background: ${({ theme }) => theme.color.white};
 
+	padding: 1rem 1.5rem;
+	@media (${props => props.theme.media.mobile_large}) {
+		padding: 2rem 3rem;
+	}
 `;
 
 export const Part = styled.div`
 
+`;
+
+
+/*
+ * Route
+ */
+export const RouteList = styled.ul`
+	list-style-type: none;
+	margin: 0;
+	padding: 0;
+
+	display: flex;
+	flex-direction: row;
+`;
+
+export const RouteItem = styled.li`
+	margin: 0 0 0 2rem;
+
+	&:first-child {
+		margin: 0;
+	}
+`;
+
+export const RouteLink = styled.a<{ $is_active: boolean }>`
+	text-decoration: ${({ $is_active }) => $is_active ? 'underline' : 'none'};
+`;
+
+export const Heading3 = styled.h3`
+	font-size: 1.4rem;
+`;
+
+
+/*
+ * Lang
+ */
+export const LangList = styled.ul`
+	list-style-type: none;
+	margin: 0;
+	padding: 0;
+
+	display: flex;
+	flex-direction: row;
+`;
+
+export const LangItem = styled.li`
+	margin: 0 2rem 0 0;
+
+	&:last-child {
+		margin: 0;
+	}
+`;
+
+export const LangLink = styled.a<{ is_active: boolean }>`
+	text-decoration: ${({ is_active }) => is_active ? 'underline' : 'none'};
+
+	cursor: pointer;
+	font-size: 1.2rem;
+
+	${({ is_active }) => is_active && (
+		css`
+			font-weight: 700;
+			color: blue;
+		`
+	)}
 `;
