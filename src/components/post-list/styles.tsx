7f6bea704062e57e831180b7c638b4e5e15097ab
François Moine <francois.moine@epitech.eu>
2021-02-22 22:54:08 +0100

    [*] add small css to post-list

--- src/components/post-list/styles.tsx
+++ src/components/post-list/styles.tsx
@@ -2,17 +2,24 @@
 import styled from 'styled-components';
 
 export const PostList = styled.ul`
+	margin: 0;
+	padding: 0 2rem;
 
+	@media(${props => props.theme.media.mobile_large}) {
+		padding: 0 4rem;
+	}
 `;
 
 export const PostItem = styled.li`
-
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
 `;
 
-export const Title = styled.div`
-
+export const Title = styled.h3`
+	text-align: center;
 `;
 
-export const Body = styled.div`
+export const Body = styled.p`
 
 `;
