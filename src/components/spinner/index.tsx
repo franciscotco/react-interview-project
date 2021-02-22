ee0982d47bb9680b17228363a5f6a43e809983a0
François Moine <francois.moine@epitech.eu>
2021-02-22 23:18:54 +0100

    [+] create spinner component

---
+++ src/components/spinner/index.tsx
@@ -0,0 +1,28 @@
+/*
+ * Dependencies
+ */
+
+// Vendors
+import React from 'react';
+
+// Styles
+import { Root, Segment } from './styles';
+
+function Spinner () {
+
+	const segments = [];
+
+	for (let i = 0; i < 12; i++) {
+		segments.push(
+			<Segment key={i} color="gray" />
+		);
+	}
+
+	return (
+		<Root>
+			{segments}
+		</Root>
+	);
+}
+
+export default Spinner;
