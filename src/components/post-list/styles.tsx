91432c6311a28445876cf72851847fc68c782dfa
François Moine <francois.moine@epitech.eu>
2021-02-23 14:24:18 +0100

    [*] reshape post-list component

--- src/components/post-list/styles.tsx
+++ src/components/post-list/styles.tsx
@@ -2,23 +2,39 @@
 import styled from 'styled-components';
 
 export const PostList = styled.ul`
-	margin: 0;
-	padding: 0 2rem;
+	padding: 0;
 
+	margin: 0 2rem;
 	@media(${props => props.theme.media.mobile_large}) {
-		padding: 0 4rem;
+		margin: 0 4rem;
 	}
+
+	overflow: hidden;
+	border-radius: .6rem;
+	border: 1px solid ${({ theme }) => theme.color.gray_medium};
 `;
 
 export const PostItem = styled.li`
+	padding: 1rem;
+
 	display: flex;
 	flex-direction: column;
 	justify-content: space-between;
+
+	background-color: ${({ theme }) => theme.color.white};
+
+	border-bottom: 1px solid ${({ theme }) => theme.color.gray_lighter};
+	&:last-child: {
+		border-bottom: unset;
+	}
 `;
 
 export const Title = styled.h3`
 	text-align: center;
+
+	font-size: 1.4rem;
 `;
 
 export const Body = styled.p`
+	font-size: 1.3rem;
 `;
